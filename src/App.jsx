4df8a9e5a70d3a91e134c30bc8bb7e22bb708d13
julius <nyerereoffice@gmail.com>
2024-05-01 06:08:24 +0300

    form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import './styles.css'
-
+import "./styles.css";
 
 const App = () => {
-  return "Hi"
-}
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="">New Item</label>
+          <input type="text" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
 
-export default App
+      
+    </>
+  );
+};
+
+export default App;
