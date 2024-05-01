a1d32ac1da9f441f6dfc33ae8813931425b0e107
julius <nyerereoffice@gmail.com>
2024-05-01 08:08:56 +0300

    Code Refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,53 @@
+import { useState } from "react";
 import "./styles.css";
+import NewTodoForm from "./components/NewTodoForm";
 
 const App = () => {
+
+  const [todos, setTodos] = useState([])
+
+
+  function toggleTodo(id, completed){
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id){
+          return {...todo, completed}
+        }
+
+        return todo
+      })
+    })
+  }
+
+
+  function deleteTodo(id){
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
   return (
     <>
-      <form className="new-item-form">
-        <div className="form-row">
-          <label htmlFor="">New Item</label>
-          <input type="text" id="item" />
-        </div>
-        <button className="btn">Add</button>
-      </form>
+      
+      <NewTodoForm />
 
       <h1 className="header">Todo List</h1>
       <ul className="list">
-        <li>
-          <label htmlFor="">
-            <input type="checkbox" />
-            item 1
-          </label>
 
-          <button className="btn btn-danger">Delete</button>
-        </li>
+        {todos.length === 0 && "No Todos"}
+        {todos.map(todo => {
+              return (
+                <li key={todo.id}>
+                  <label>
+                    <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)} />
+                    {todo.title}
+                  </label>
+
+                  <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
+                </li>
+              )
+        })}
+        
       </ul>
     </>
   );
